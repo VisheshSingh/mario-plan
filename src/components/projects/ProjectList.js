f0a2607a969715612c2502afa6340c807187a88f
Vishesh <visheshsingh16@gmail.com>
2018-12-09 21:51:08 -0500

    Connect to redux store and grabbing dummy data

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
-  return (
-    <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-    </div>
+const ProjectList = props => {
+  const { projects } = props;
+  let projectList = projects.length ? (
+    projects.map(project => {
+      return <ProjectSummary project={project} key={project.id} />;
+    })
+  ) : (
+    <p>No projects at this time...</p>
   );
+  return <div className="project-list section">{projectList}</div>;
 };
 
 export default ProjectList;
